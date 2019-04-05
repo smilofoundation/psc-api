160d429f7682de22abf40fde27ac8d2a07fe1fc9
Thomas Modeneis <thomas.modeneis@gmail.com>
2019-04-05 19:11:13 +0200

    add register scripts add bunyan logs add joi schema validation add nedb database and save psc contracts

---
+++ readme.md
@@ -0,0 +1,1 @@
+# Smilo Biometrics DEMO
--- server.js
+++ server.js
@@ -1,71 +1,144 @@
 const Web3 = require('web3');
 const fs = require('fs');
+const Joi = require('joi');
+const bunyan = require('bunyan');
+const log = bunyan.createLogger({name: "srv"});
 
 let web3 = new Web3(new Web3.providers.HttpProvider("http://localhost:22000"));
 
 let abiDefinition = fs.readFileSync('./simplestorage_sol_SimpleStorage.abi').toString();
 abiDefinition = JSON.parse(abiDefinition);
 
-const lastBlockStr = process.env.LASTBLOCK || "0";
-let lastBlock = parseInt(lastBlockStr);
+let LASTBLOCK = parseInt(process.env.LASTBLOCK || "0");
+let TIMEOUT = parseInt(process.env.TIMEOUT || "5000");
 
+let IDENTITY_TIMEOUT = parseInt(process.env.IDENTITY_TIMEOUT || "172800"); // 48h
+
+const SCHEMA = Joi.object().keys({
+    version: Joi.string().required(),
+    name: Joi.string().required(),
+    biometrics: Joi.array(),
+    createdAt: Joi.date().required(),
+});
+
+const Datastore = require('nedb');
+const IDENTITY_DB = new Datastore({
+    inMemoryOnly: true
+});
+
+IDENTITY_DB.ensureIndex({fieldName: 'createdAt', expireAfterSeconds: IDENTITY_TIMEOUT}, function (err) {
+    log.error({err: err}, "Could not create index");
+});
 
 function parseBlock(i, b, cb) {
-    if (i < b.transactions.length) {
-        // for (let i = 0; i < b.transactions.length; i++) {
-        const txTarget = b.transactions[i];
+    const txTarget = b.transactions[i];
+    if (txTarget) {
         i = i + 1;
 
         web3.eth.getTransaction(txTarget).then(function (tx) {
             // check if private
             if (tx.v == 37 || tx.v == 38) {
-                console.log("Got a private transaction", tx);
+                log.info({"tx": tx.hash}, "Got a private transaction");
             } else {
                 return parseBlock(i, b, cb); // skip public transactions
             }
 
             //load smart contract
-            console.log("Found a private contract, will load receipt");
+            log.info({"tx": tx.hash}, "Found a private contract, will load receipt");
             web3.eth.getTransactionReceipt(tx.hash, function (err, receipt) {
                 if (err) {
-                    console.log("Coult not getTransactionReceipt, ", err);
+                    log.error({
+                        err: err,
+                        "receipt.contractAddress": receipt.contractAddress,
+                        "tx": tx.hash,
+                    }, "Coult not getTransactionReceipt");
                     return parseBlock(i, b, cb);
                 }
-                console.log("Found a private contract, will load it using receipt");
+                log.info({
+                    "tx": tx.hash,
+                    "receipt.contractAddress": receipt.contractAddress,
+                }, "Found a private contract, will load it, ");
                 const contract = new web3.eth.Contract(abiDefinition, receipt.contractAddress);
 
                 contract.methods.get().call().then(function (val) {
-                    console.log("****** Got a val from private contract, ", val);
+                    log.info({
+                        "receipt.contractAddress": receipt.contractAddress,
+                        "tx": tx.hash,
+                    }, "****** Got a val from private contract, ");
+
+                    let identity = null;
+                    try {
+                        identity = JSON.parse(val);
+                    } catch (err) {
+                        log.error({
+                            err: err,
+                            "receipt.contractAddress": receipt.contractAddress,
+                            "tx": tx.hash,
+                        }, "Could not parse JSON inside this psc, ");
+                        return parseBlock(i, b, cb);
+                    }
+
+                    identity.createdAt = new Date();
+
+                    const isValid = SCHEMA.validate(identity);
+                    if (!isValid) {
+                        log.error({
+                            err: err,
+                            "receipt.contractAddress": receipt.contractAddress,
+                            "tx": tx.hash,
+                        }, "Could not validate JSON inside this psc, ");
+                        return parseBlock(i, b, cb);
+                    }
+
+                    IDENTITY_DB.insert(identity, function (err, newDoc) {
+                        if (err) {
+                            log.error({
+                                err: err,
+                                "receipt.contractAddress": receipt.contractAddress,
+                                "tx": tx.hash,
+                            }, "Failed to save data in memory ...");
+                        } else {
+                            log.info({
+                                "_id": newDoc._id,
+                                "receipt.contractAddress": receipt.contractAddress,
+                                "tx": tx.hash,
+                            }, "Saved data in memory ...");
+                        }
+                    });
+
                     return parseBlock(i, b, cb);
                 }).catch(function (err) {
-                    console.log("Could not load this contract, probably not the right one");
+                    log.error({
+                        err: err,
+                        "receipt.contractAddress": receipt.contractAddress,
+                        "tx": tx.hash,
+                    }, "Could not load this contract, probably not the right one");
                     return parseBlock(i, b, cb);
                 });
             }).catch(function (err) {
-                console.log("Could not load getTransactionReceipt");
+                log.error({err: err, "tx": tx.hash}, "Could not load getTransactionReceipt");
                 return parseBlock(i, b, cb);
             });
 
         }).catch(function (err) {
-            console.log("Could not load getTransaction");
+            log.error({err: err, "tx": txTarget}, "Could not load getTransaction");
             return parseBlock(i, b, cb);
         });
     } else {
-        console.log("Finished processing block ", b.number);
+        log.info({"number": b.number}, "Finished processing block ");
         return cb();
     }
-
-    // }
 }
 
 function scanBlock(blockNumber, cb) {
     web3.eth.getBlock(blockNumber).then(function (b) {
 
         parseBlock(0, b, function () {
-            return cb()
-        })
+            return cb();
+        });
+
     }).catch(function (err) {
-        console.log("Could not load getBlock");
+        log.error({err: err}, "Could not load getBlock");
         return cb();
     });
 
@@ -74,33 +147,46 @@ function scanBlock(blockNumber, cb) {
 
 function doJob() {
 
+    IDENTITY_DB.count({}, function (err, count) {
+        if (err) {
+            log.error({err: err}, "Failed to get the total of identities in memory, ");
+        } else {
+            log.info({"count": count}, "Counting identities saved in memory ... ");
+        }
+    });
+
     web3.eth.getBlockNumber().then(function (blockNumber) {
         if (!blockNumber) {
-            console.log("Could not get blockNumber, are you connected ?", web3.eth);
+            log.error("Could not get blockNumber, are you connected ?");
+            setTimeout(function () {
+                return doJob();
+            }, TIMEOUT);
         } else {
-            if (lastBlock <= blockNumber) {
+            if (LASTBLOCK <= blockNumber) {
                 processTransactions(blockNumber);
             }
 
-            console.log("restarting, ", "blockNumber", blockNumber, "lastBlock", lastBlock);
+            log.info({"blockNumber": blockNumber, "LASTBLOCK": LASTBLOCK}, "restarting, ");
 
             setTimeout(function () {
-                doJob();
-            }, 5000);
+                return doJob();
+            }, TIMEOUT);
 
         }
     }).catch(function (err) {
-        console.log("Could not load doJob, getBlockNumber");
-        return
+        log.error({err: err}, "Could not load getBlockNumber");
+        setTimeout(function () {
+            return doJob();
+        }, TIMEOUT);
     });
 }
 
 function processTransactions(blockNumber) {
-    if (lastBlock <= blockNumber) {
-        console.log("Scanning blockNumber, ", lastBlock);
-        scanBlock(lastBlock, function () {
+    if (LASTBLOCK <= blockNumber) {
+        log.info({"LASTBLOCK": LASTBLOCK}, "Scanning blockNumber, ");
+        scanBlock(LASTBLOCK, function () {
 
-            lastBlock = lastBlock + 1;
+            LASTBLOCK = LASTBLOCK + 1;
 
             processTransactions(blockNumber);
         });
